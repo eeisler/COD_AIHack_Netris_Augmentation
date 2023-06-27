1af08f5c8db3af1fc72cc1000876c05a7a83e232
eeisler <eeislerr@gmail.com>
2023-06-27 10:41:40 +0500

    Complete augmentation

--- augmentation.py
+++ augmentation.py
@@ -6,8 +6,8 @@ import numpy as np
 import torch
 import torchvision.transforms as T
 
-jpg_input_folder = input("Enter the path to images input folder: ")
-txt_input_folder = input("Enter the path to labels input folder: ")
+jpg_input_path = input("Enter the path to input images folder: ")
+txt_input_path = input("Enter the path to input labels folder: ")
 
 downloads_folder = os.path.expanduser(r"~\Downloads")
 
@@ -19,30 +19,6 @@ jpg_result_folder = r'results\images'
 jpg_result_path = os.path.join(downloads_folder, jpg_result_folder)
 os.makedirs(jpg_result_path, exist_ok=True)
 
-def plot(imgs, with_orig=False, row_title=None, **imshow_kwargs):
-    if not isinstance(imgs[0], list):
-        imgs = [imgs]
-
-    num_rows = len(imgs)
-    num_cols = len(imgs[0]) + with_orig
-    fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, squeeze=False)
-    for row_idx, row in enumerate(imgs):
-        row = [orig_img] + row if with_orig else row
-        for col_idx, img in enumerate(row):
-            ax = axs[row_idx, col_idx]
-            ax.imshow(np.asarray(img), **imshow_kwargs)
-            ax.set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
-
-    if with_orig:
-        axs[0, 0].set(title='Original image')
-        axs[0, 0].title.set_size(8)
-    if row_title is not None:
-        for row_idx in range(num_rows):
-            axs[row_idx, 0].set(ylabel=row_title[row_idx])
-
-    plt.tight_layout()
-    plt.close()
-
 
 def folder_save(file_name, imgs, transformation_type):
     if isinstance(imgs, list):
@@ -50,17 +26,16 @@ def folder_save(file_name, imgs, transformation_type):
             img_path = f"{jpg_result_path}/{file_name[:-4]}.{i + 1}.{transformation_type}.jpg"
             img.save(img_path)
 
-        print(f"Saved {len(imgs)} images.")
+        print(f"Saved {len(imgs)} images")
     else:
         img_path = f"{jpg_result_path}/{file_name[:-4]}.1.{transformation_type}.jpg"
         imgs.save(img_path)
-        print("Saved 1 image.")
+        print("Saved 1 image")
 
 
 def gray(orig_img, file_name):
     transformation_type = 'gray'
     gray_img = T.Grayscale()(orig_img)
-    plot([gray_img], cmap='gray')
     folder_save(file_name, gray_img, transformation_type)
 
 
@@ -68,7 +43,6 @@ def jitter(orig_img, file_name):
     transformation_type = 'filter'
     jitter = T.ColorJitter(brightness=.5, hue=.3)
     jitted_imgs = [jitter(orig_img) for _ in range(4)]
-    plot(jitted_imgs)
     folder_save(file_name, jitted_imgs, transformation_type)
 
 
@@ -76,7 +50,6 @@ def blur(orig_img, file_name):
     transformation_type = 'blur'
     blurrer = T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))
     blurred_imgs = [blurrer(orig_img) for _ in range(4)]
-    plot(blurred_imgs)
     folder_save(file_name, blurred_imgs, transformation_type)
 
 
@@ -84,7 +57,6 @@ def post(orig_img, file_name):
     transformation_type = 'post'
     posterizer = T.RandomPosterize(bits=2)
     posterized_imgs = [posterizer(orig_img) for _ in range(4)]
-    plot(posterized_imgs)
     folder_save(file_name, posterized_imgs, transformation_type)
 
 
@@ -92,7 +64,6 @@ def sharp(orig_img, file_name):
     transformation_type = 'sharp'
     sharpness_adjuster = T.RandomAdjustSharpness(sharpness_factor=2)
     sharpened_imgs = [sharpness_adjuster(orig_img) for _ in range(4)]
-    plot(sharpened_imgs)
     folder_save(file_name, sharpened_imgs, transformation_type)
 
 
@@ -100,7 +71,6 @@ def equalizer(orig_img, file_name):
     transformation_type = 'equalizer'
     equalizer = T.RandomEqualize()
     equalized_imgs = [equalizer(orig_img) for _ in range(4)]
-    plot(equalized_imgs)
     folder_save(file_name, equalized_imgs, transformation_type)
 
 
@@ -108,22 +78,21 @@ def flipper(orig_img, file_name):
     transformation_type = 'flipper'
     hflipper = T.RandomHorizontalFlip(p=0.5)
     transformed_imgs = [hflipper(orig_img) for _ in range(4)]
-    plot(transformed_imgs)
     folder_save(file_name, transformed_imgs, transformation_type)
 
 
 def txt_change(txt_input_path):
+    jpg_result_file_names = os.listdir(jpg_result_path)
     txt_file_names = os.listdir(txt_input_path)
-    counter = 0
 
-    for jpg_file in sorted(jpg_file_names):
-        for txt_file in sorted(txt_file_names):
+    for jpg_file in sorted(jpg_result_file_names):
+        for txt_file in txt_file_names:
             old_txt_name = txt_file.split(".")[0]
             new_txt_name = os.path.splitext(jpg_file)[0]
 
             if old_txt_name == new_txt_name.split('.')[0]:
-                counter += 1
                 txt_file_path = os.path.join(txt_input_path, txt_file)
+
                 with open(txt_file_path, 'r') as txt_file:
                     txt_data = txt_file.read()
 
@@ -132,16 +101,15 @@ def txt_change(txt_input_path):
 
                 with open(new_txt_path, 'w') as new_txt_file:
                     new_txt_file.write(txt_data)
-    print(counter)
 
 
-jpg_file_names = os.listdir(jpg_input_folder)
+jpg_input_file_names = os.listdir(jpg_input_path)
 
-for file_name in jpg_file_names:
-    jpg_file_path = os.path.join(jpg_input_folder, file_name)
+for file_name in jpg_input_file_names:
+    jpg_file_path = os.path.join(jpg_input_path, file_name)
     if not os.path.isfile(jpg_file_path):
         continue
-    print(f"{file_name} file in progress\n")
+    print(f"\n{file_name} file in progress\n")
     orig_img = Image.open(jpg_file_path)
     torch.manual_seed(0)
 
@@ -153,4 +121,14 @@ for file_name in jpg_file_names:
     equalizer(orig_img, file_name)
     flipper(orig_img, file_name)
 
-txt_change(txt_input_folder)
+txt_change(txt_input_path)
+
+jpg_result_files = os.listdir(jpg_result_path)
+jpg_file_count = len(jpg_result_files)
+
+txt_result_files = os.listdir(txt_result_path)
+txt_file_count = len(txt_result_files)
+
+print(f"Images saved: {jpg_file_count}")
+print(f"Labels saved: {txt_file_count}")
+
